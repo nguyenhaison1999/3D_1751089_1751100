11a815494539afe638a7ff01a128b5703c46bfbb
Nguyen Hai-Son <nhson17@apcs.vn>
2020-09-26 17:00:44 +0700

    Add coins

--- unity3d_final/Assets/Scripts/PlayerManager.cs
+++ unity3d_final/Assets/Scripts/PlayerManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
@@ -10,12 +11,16 @@ public class PlayerManager : MonoBehaviour
     public static bool isGameStarted;
     public GameObject startingText;
 
+    public static int numberOfCoins;
+    public Text coinsText;
+
     // Start is called before the first frame update
     void Start()
     {
         gameOver = false;
         Time.timeScale = 1;
         isGameStarted = false;
+        numberOfCoins = 0; 
     }
 
     // Update is called once per frame
@@ -26,6 +31,8 @@ public class PlayerManager : MonoBehaviour
             Time.timeScale = 0;
             gameOverPanel.SetActive(true);
         }
+
+        coinsText.text = "Coins: " + numberOfCoins;
 
         if (SwipeManager.tap)
         {
